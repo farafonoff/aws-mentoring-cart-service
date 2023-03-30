84ab03c371bc1ec2db99393cc3db8753639b8330
Artem Farafonov <artem_farafonov@epam.com>
2023-03-30 23:53:10 +0300

    update nest and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 const port = process.env.PORT || 4000;
@@ -10,6 +10,15 @@ async function bootstrap() {
   app.enableCors({
     origin: (req, callback) => callback(null, true),
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(port);
 }
--- src/mainLambda.ts
+++ src/mainLambda.ts
@@ -1,5 +1,5 @@
 import { configure as serverlessExpress } from '@vendia/serverless-express';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
@@ -8,10 +8,19 @@ let cachedServer;
 
 export const handler = async (event, context) => {
   if (!cachedServer) {
-    const nestApp = await NestFactory.create(AppModule);
-    await nestApp.init();
+    const app = await NestFactory.create(AppModule);
+    const config = new DocumentBuilder()
+      .setTitle('Cats example')
+      .setDescription('The cats API description')
+      .setVersion('1.0')
+      .addTag('cats')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
+    await app.init();
     cachedServer = serverlessExpress({
-      app: nestApp.getHttpAdapter().getInstance(),
+      app: app.getHttpAdapter().getInstance(),
     });
   }
 
