d4661e5fdcfc857af4506cd773ccd2995b5625e8
Artem Farafonov <artem_farafonov@epam.com>
2023-03-30 23:52:56 +0300

    update nest and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 
-import * as helmet from 'helmet';
-
 import { AppModule } from './app.module';
 
 const port = process.env.PORT || 4000;
@@ -12,11 +10,9 @@ async function bootstrap() {
   app.enableCors({
     origin: (req, callback) => callback(null, true),
   });
-  app.use(helmet());
 
   await app.listen(port);
 }
 bootstrap().then(() => {
   console.log('App is running on %s port', port);
 });
-
